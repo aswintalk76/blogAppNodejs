b9bdd451421e6bdaf776e4afb3af9b38bb5d6c64
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-18 01:37:27 +0530

    create a server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("working on blogappnodejs")
+const express = require("express");
+const app = express();
+const port = process.env.port || 5000;
+
+//create a default route
+app.get("/", (req, res) => {
+    res.send(`Server is working on ${port}`);
+});
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`);
+})
