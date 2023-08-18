d7efeeb2e4a22b05fcda479d24180f21448fda27
Ashwani Raj Agrahari <77192787+aswintalk76@users.noreply.github.com>
2023-08-18 14:34:56 +0530

    defined morgan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const morgan = require("morgan");
 const connectDB = require("./config/db");
 
 //env config
