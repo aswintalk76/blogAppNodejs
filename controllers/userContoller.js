5f1058bfb855f815d386b5bb6a25d697f19499e3
Ashwani <aswintalk76@gmail.com>
2023-08-18 17:04:29 +0530

    add registerController userController.js

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -3,23 +3,57 @@ const bcrypt = require("bcrypt");
 
 //create user register user
 exports.registerController = async (req, res) => {
-    return res.status(201).send({
-        success: true,
-        message: "New User Created",
-    });
+  // return res.status(201).send({
+  //     success: true,
+  //     message: "New User Created",
+  // });
+  try {
+    const { username, email, password } = req.body;
+    //validation
+    if (!username || !email || !password) {
+      return res.status(400).send({
+        success: false,
+        message: "Please Fill all fields",
+      });
+    }
+    //exisiting user
+    const exisitingUser = await userModel.findOne({ email });
+    if (exisitingUser) {
+      return res.status(401).send({
+        success: false,
+        message: "user already exisits",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    //save new user
+    const user = new userModel({ username, email, password: hashedPassword });
+    await user.save();
+    return res.status(201).send({
+      success: true,
+      message: "New User Created",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Error In Register callback",
+      success: false,
+      error,
+    });
+  }
 };
 // get all users
 exports.getAllUsers = async (req, res) => {
-    return res.status(200).send({
-        success: true,
-        message: "All User Data",
-    });
+  return res.status(200).send({
+    success: true,
+    message: "All User Data",
+  });
 };
 //login
 exports.loginController = async (req, res) => {
-    return res.status(200).send({
-        success: true,
-        message: "login successfully",
-    });
+  return res.status(200).send({
+    success: true,
+    message: "login successfully",
+  });
 };
