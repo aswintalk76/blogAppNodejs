b66234658d15546841890e2d0b6a4d35e3e75004
Ashwani <aswintalk76@gmail.com>
2023-08-18 17:08:15 +0530

    add loginController userController.js

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -52,8 +52,45 @@ exports.getAllUsers = async (req, res) => {
 };
 //login
 exports.loginController = async (req, res) => {
-  return res.status(200).send({
-    success: true,
-    message: "login successfully",
-  });
+  //   return res.status(200).send({
+  //     success: true,
+  //     message: "login successfully",
+  //   });
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(401).send({
+        success: false,
+        message: "Please provide email or password",
+      });
+    }
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(200).send({
+        success: false,
+        message: "email is not registerd",
+      });
+    }
+    //password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send({
+        success: false,
+        message: "Invlid username or password",
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      messgae: "login successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error In Login Callcback",
+      error,
+    });
+  }
 };
