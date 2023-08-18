75deff7f2d6e72b2c354c3fbaec8e170159ef8bd
Ashwani <aswintalk76@gmail.com>
2023-08-18 18:34:02 +0530

    createBlogController

--- controllers/blogControlller.js
+++ controllers/blogControlller.js
@@ -12,10 +12,35 @@ exports.getAllBlogsController = async (req, res) => {
 
 //Create Blog
 exports.createBlogController = async (req, res) => {
-  return res.status(200).send({
-    success: true,
-    message: "Blog Created!",
-  });
+  // return res.status(200).send({
+  //   success: true,
+  //   message: "Blog Created!",
+  // });
+  try {
+    const { title, description, image, user } = req.body;
+    //validation
+    if (!title || !description || !image || !user) {
+      return res.status(400).send({
+        success: false,
+        message: "Please Provide ALl Fields",
+      });
+    }
+
+    const newBlog = new blogModel({ title, description, image, user });
+    await newBlog.save();
+    return res.status(201).send({
+      success: true,
+      message: "Blog Created!",
+      newBlog,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+      message: "Error WHile Creting blog",
+      error,
+    });
+  }
 };
 
 //Update Blog
