1b64983809d1783433e399b020bbfe98563cf0de
Ashwani <aswintalk76@gmail.com>
2023-08-18 18:57:24 +0530

    getAllBlogsController

--- controllers/blogControlller.js
+++ controllers/blogControlller.js
@@ -4,10 +4,32 @@ const userModel = require("../models/userModel");
 
 //GET ALL BLOGS
 exports.getAllBlogsController = async (req, res) => {
-  return res.status(200).send({
-    success: true,
-    message: "All Blogs lists",
-  });
+  // return res.status(200).send({
+  //   success: true,
+  //   message: "All Blogs lists",
+  // });
+  try {
+    const blogs = await blogModel.find({});
+    if (!blogs) {
+      return res.status(200).send({
+        success: false,
+        message: "No Blogs Found",
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      BlogCount: blogs.length,
+      message: "All Blogs lists",
+      blogs,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error WHile Getting Blogs",
+      error,
+    });
+  }
 };
 
 //Create Blog
