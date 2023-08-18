f6562cecbe5058c748c89b996b5af8f2b826b239
Ashwani <aswintalk76@gmail.com>
2023-08-18 18:41:45 +0530

    getBlogByIdController

--- controllers/blogControlller.js
+++ controllers/blogControlller.js
@@ -74,10 +74,32 @@ exports.updateBlogController = async (req, res) => {
 
 //SIngle Blog
 exports.getBlogByIdController = async (req, res) => {
-  return res.status(200).send({
-    success: true,
-    message: "fetch single blog",
-  });
+  // return res.status(200).send({
+  //   success: true,
+  //   message: "fetch single blog",
+  // });
+  try {
+    const { id } = req.params;
+    const blog = await blogModel.findById(id);
+    if (!blog) {
+      return res.status(404).send({
+        success: false,
+        message: "blog not found with this is",
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      message: "fetch single blog",
+      blog,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      success: false,
+      message: "error while getting single blog",
+      error,
+    });
+  }
 };
 
 //Delete Blog
